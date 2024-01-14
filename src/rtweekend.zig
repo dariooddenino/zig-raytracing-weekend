4a3282c1e6ad1d480f065f94af959826dd373b39
Dario Oddenino <branch13@gmail.com>
2024-01-14 20:37:04 +0100

    random?

--- src/rtweekend.zig
+++ src/rtweekend.zig
@@ -6,3 +6,17 @@ pub const pi: f32 = 3.1415926535897932385;
 pub inline fn degrees_to_radians(degrees: f32) f32 {
     return degrees * pi / 180.0;
 }
+
+pub inline fn randomDouble() f32 {
+    return std.crypto.random.float(f32);
+}
+
+pub inline fn randomDoubleRange(min: f32, max: f32) f32 {
+    return min + (max - min) * randomDouble();
+}
+
+// TODO: Do we have property testing in zig?
+test "randomDouble" {
+    const val = randomDouble();
+    try std.testing.expect(val > 0 and val < 1);
+}
